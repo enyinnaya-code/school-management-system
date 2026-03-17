ee4d824a7c694d8a68962a8f75d18b275ebd5758
enyinnaya <enyinnayaosuoha@gmail.com>
2026-03-17 04:11:02 +0100

    result access added

--- resources/views/results/settings/index.blade.php
+++ resources/views/results/settings/index.blade.php
@@ -1,16 +1,19 @@
 @include('includes.head')
 
 <body>
-<div class="loader"></div>
-<div id="app">
-    <div class="main-wrapper main-wrapper-1">
-        <div class="navbar-bg"></div>
-        @include('includes.right_top_nav')
-        @include('includes.side_nav')
-
-       
+    <div class="loader"></div>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            @include('includes.right_top_nav')
+            @include('includes.side_nav')
+            <!-- Main Content -->
+            <div class="main-content pt-5 mt-5">
+                
+            </div>
+        </div>
+        @include('includes.edit_footer')
     </div>
-</div>
 
-@include('includes.edit_footer')
-
+   
+</body>
